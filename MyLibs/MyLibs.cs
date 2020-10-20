46013ce577ab746f45c5ad1145aad1a7254d16ba
nickahickman <nickahickman@gmail.com>
2020-10-20 16:24:18 -0400

    initial commit

---
+++ MyLibs/MyLibs.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MyLibs
+{
+    public class UserInputLibrary
+    {
+        public static string GetUserResponse(string prompt)
+        {
+            string userResponse;
+
+            Console.WriteLine(prompt);
+            userResponse = Console.ReadLine().Trim();
+
+            while (String.IsNullOrEmpty(userResponse))
+            {
+                Console.WriteLine("I need you to enter something. Silence isn't a virtue here!");
+                userResponse = GetUserResponse(prompt);
+            }
+
+            return userResponse;
+        }
+
+        public static bool UserWantsToContinue(string originalQuery, string failResponse)
+        {
+            string userResponse = GetUserResponse($"{originalQuery}? (y/n)").ToLower(); ;
+
+            while (userResponse != "n" && userResponse != "y")
+            {
+                userResponse = GetUserResponse($"{failResponse} {originalQuery} (y/n)").ToLower();
+            }
+
+            if (userResponse == "n")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+    public class ConsoleLibrary
+    {
+        public static void DrawProgramTitle(string title)
+        {
+            Console.WriteLine(title);
+            DrawSeparator();
+        }
+        public static void DrawSeparator()
+        {
+            Console.WriteLine("=========================================================================");
+        }
+    }
+
+    public class GTKYC
+    {
+        public static void PrintAllStudents(string[,] students)
+        {
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+                ConsoleLibrary.DrawSeparator();
+            }
+        }
+
+        public static string GetAllStudentProperties(string[] student)
+        {
+            string studentInfo = "";
+
+            for (int i = 0; i < student.Length; i++)
+            {
+                if (i == 0)
+                {
+                    studentInfo += $"Name: {student[0]}";
+                }
+                else if (i == 1)
+                {
+                    studentInfo += $"Favorite food: {student[1]}";
+                }
+                else {
+                    studentInfo += $"Hometown: {student[2]}";
+                }
+            }
+
+            return studentInfo;
+        }
+    }
+}
---
+++ RegexRedux/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace RegexRedux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userEmail;
+            string userPhoneNumber;
+            string userDate;
+
+            Console.WriteLine("Gimme all your info for very legal and legitimate purposes");
+            MyLibs.ConsoleLibrary.DrawSeparator();
+
+            while (true)
+            {
+                userName = MyLibs.UserInputLibrary.GetUserResponse("What's your name? Must begin with a capital letter and be fewer than 30 characters");
+                while (!MyLibs.InputValidationLibrary.IsValidName(userName))
+                {
+                    userName = MyLibs.UserInputLibrary.GetUserResponse("That's an invalid name, try again.");
+                }
+
+                userEmail = MyLibs.UserInputLibrary.GetUserResponse("What's your email address? ex: name@mail.co");
+                while (!MyLibs.InputValidationLibrary.IsValidEmail(userEmail))
+                {
+                    userEmail = MyLibs.UserInputLibrary.GetUserResponse("That's an invalid email, try again. ex: name@mail.co");
+                }
+
+                userPhoneNumber = MyLibs.UserInputLibrary.GetUserResponse("What's your phone number? xxx-xxx-xxxx");
+                while (!MyLibs.InputValidationLibrary.IsValidPhoneNumber(userPhoneNumber))
+                {
+                    userPhoneNumber = MyLibs.UserInputLibrary.GetUserResponse("That's an invalid phone number, try again.");
+                }
+
+                userDate = MyLibs.UserInputLibrary.GetUserResponse("What's your birthdate? dd/mm/yyyy");
+                while (!MyLibs.InputValidationLibrary.IsValidDate(userDate))
+                {
+                    userDate = MyLibs.UserInputLibrary.GetUserResponse("That's an invalid date, try again. dd/mm/yyyy");
+                }
+
+                if (!MyLibs.UserInputLibrary.UserWantsToContinue("Thanks! Want to give me someone else's information?", "I didn't understand that."))
+                {
+                    Console.WriteLine("Thanks for the info, I'll put it to very good use ;)");
+                    break;
+                }
+            }
+        }
+    }
+}
